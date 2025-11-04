16c922980d2d73475c22d251e588e9a49ed6d2b5
GabrielUzel <gabriel.uzel1@gmail.com>
2025-11-04 17:45:40 -0300

    Fix deploy bug

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 set -e
 
 echo "Waiting for PostgreSQL to become available..."
 
-while ! pg_isready -h db -p 5432 -U $POSTGRES_USERNAME > /dev/null 2>&1; do
+while ! nc -z db 5432; do
   echo "Waiting for database..."
   sleep 2
 done
@@ -10,10 +11,10 @@ done
 echo "PostgreSQL is available!"
 
 echo "Creating database if it doesn't exist..."
-/app/_build/prod/rel/your_app/bin/your_app eval "YourApp.Release.create_database()" || echo "Database already exists or couldn't be created"
+/app/_build/prod/rel/backend/bin/backend eval "Backend.Release.create_database()" || echo "Database already exists or couldn't be created"
 
 echo "Running migrations..."
-/app/_build/prod/rel/your_app/bin/your_app eval "YourApp.Release.migrate()"
+/app/_build/prod/rel/backend/bin/backend eval "Backend.Release.migrate()"
 
 echo "Starting application..."
 exec "$@"
